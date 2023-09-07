18dc29d55cbb9973d19d068751936e5b2900742a
user <melany.torres@unah.hn>
2023-09-06 20:34:15 -0600

    Creacion de DataTable Solicitud

---
+++ Vista/crud/DataTableSolicitud/obtenerDataTableSolicitud.php
@@ -0,0 +1,9 @@
+<?php
+   require_once ("../../../db/Conexion.php");
+   require_once ("../../../Modelo/DataTableSolicitud.php");
+   require_once("../../../Controlador/ControladorDataTableSolicitud.php");
+   session_start();
+   if(isset($_SESSION['usuario'])){
+      $data = ControladorDataTableSolicitud::DataTableSolicitud($_SESSION['usuario']);
+      print json_encode($data, JSON_UNESCAPED_UNICODE);
+   }
