cb258e93b9241f34df920191288a61e0fe93c3a8
danielamartinez08 <estefanimartinez@unah.hn>
2023-09-07 23:53:00 -0600

    bitacora crud cliente, cartera cliente, preguntas

--- Vista/crud/carteraCliente/editarCliente.php
+++ Vista/crud/carteraCliente/editarCliente.php
@@ -3,13 +3,13 @@
     require_once ("../../../Modelo/Usuario.php");
     require_once("../../../Controlador/ControladorUsuario.php");
     require_once ("../../../Modelo/CarteraClientes.php");
-    // require_once ("../../../Modelo/Bitacora.php");
+    require_once ("../../../Modelo/Bitacora.php");
     require_once("../../../Controlador/ControladorCarteraClientes.php");
-    // require_once("../../../Controlador/ControladorBitacora.php");
+    require_once("../../../Controlador/ControladorBitacora.php");
     
 
-    // session_start(); //Reanudamos session
-    // if(isset($_SESSION['usuario'])){
+    session_start(); //Reanudamos session
+    if(isset($_SESSION['usuario'])){
         $nuevoCliente = new CarteraClientes();
         $nuevoCliente->idcarteraCliente = $_POST['id'];
         $nuevoCliente->nombre = $_POST['nombre'];
@@ -19,19 +19,18 @@
         $nuevoCliente->direccion = $_POST['direccion'];
         $nuevoCliente->estadoContacto = $_POST['estadoContacto'];
         date_default_timezone_set('America/Tegucigalpa');
-        $nuevoCliente->modificadoPor = 'SUPERADMIN';
-        $nuevoCliente->fechaModificacion = date("Y-m-d h:i:s");
+        $nuevoCliente->modificadoPor = $_SESSION['usuario'];
         ControladorCarteraClientes::editarCliente($nuevoCliente);
         print json_encode($data = ['estado'=>'false']);
-        /* ========================= Evento Editar Usuario. ======================*/
-        // $newBitacora = new Bitacora();
-        // $accion = ControladorBitacora::accion_Evento();
-        // date_default_timezone_set('America/Tegucigalpa');
-        // $newBitacora->fecha = date("Y-m-d h:i:s"); 
-        // $newBitacora->idObjeto = ControladorBitacora:: obtenerIdObjeto('gestionUsuario.php');
-        // $newBitacora->idUsuario = ControladorUsuario::obtenerIdUsuario($_SESSION['usuario']);
-        // $newBitacora->accion = $accion['Update'];
-        // $newBitacora->descripcion = 'El usuario '.$_SESSION['usuario'].' modificó el usuario '.$_POST['usuario'];
-        // ControladorBitacora::SAVE_EVENT_BITACORA($newBitacora);
-        // /* =======================================================================================*/
-    // }
+        /* ========================= Evento Editar cartera cliente. ======================*/
+        $newBitacora = new Bitacora();
+        $accion = ControladorBitacora::accion_Evento();
+        date_default_timezone_set('America/Tegucigalpa');
+        $newBitacora->fecha = date("Y-m-d h:i:s"); 
+        $newBitacora->idObjeto = ControladorBitacora:: obtenerIdObjeto('gestionCarteraClientes.php');
+        $newBitacora->idUsuario = ControladorUsuario::obtenerIdUsuario($_SESSION['usuario']);
+        $newBitacora->accion = $accion['Update'];
+        $newBitacora->descripcion = 'El usuario '.$_SESSION['usuario'].' modificó al cliente '.$_POST['nombre'];
+        ControladorBitacora::SAVE_EVENT_BITACORA($newBitacora);
+        /* =======================================================================================*/
+    }
