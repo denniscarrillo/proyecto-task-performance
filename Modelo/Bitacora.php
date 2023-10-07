ec6fa56da3a6c750f0ee0805706d52a5e8502cae
danielamartinez08 <estefanimartinez@unah.hn>
2023-10-07 15:58:42 -0600

    Se corrio la bitacora de recuperar contraseña por pregunta

--- Modelo/Bitacora.php
+++ Modelo/Bitacora.php
@@ -36,7 +36,7 @@
                 'Update' => 'Actualizacion',
                 'Delete' => 'Eliminacion',
                 'Login'  => 'Iniciar Sesion',
-                'BloqueoPreguntas' => 'Intento recuperación contraseña',
+                'BloqueoPreguntas' => 'recup. contraseña',
                 'Logout'  => 'Cerrar Session',
                 'income'  => 'Ingreso',
                 'fallido' => 'Ingreso fallido',
