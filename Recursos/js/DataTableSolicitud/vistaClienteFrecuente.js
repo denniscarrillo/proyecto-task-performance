175b220b18d57fc7385429e1467216ea951ed96f
TaniaCastro22 <tmcastro@unah.hn>
2023-11-02 19:58:14 -0600

    guardado de solicitud

--- Recursos/js/DataTableSolicitud/vistaClienteFrecuente.js
+++ Recursos/js/DataTableSolicitud/vistaClienteFrecuente.js
@@ -349,8 +349,7 @@ $(document).on("click", "#btn_selectfactura", function () {
 
 ///////////GUARDAR NUEVA SOLICITUD
 $('#form-solicitud').submit(function (e) {
-  e.preventDefault(); 
-  
+  e.preventDefault();  
   let idFactura = $('#idfactura').val();
   let correo = $('#correo').val();
   let telefono = $('#telefono').val();
@@ -358,17 +357,15 @@ $('#form-solicitud').submit(function (e) {
   let ubicacion = $('#direccion').val();
   let descripcion = $('#descripcion').val();
   let rtncliente, rtnclienteC;
-
   var radio = document.getElementById("clienteExistente");
 if (radio.checked) {
-  rtncliente = $('#rntcliente').val() || null;
-  rtnclienteC = null; 
-} else {
-  rtnclienteC = $('#rntcliente').val() || null;
-  rtncliente = null; 
-  idFactura = null;
+rtncliente = $('#rntcliente').val();
+   rtnclienteC = 'NULL'; 
+ } else {
+rtnclienteC = $('#rntcliente').val();
+   rtncliente = 'NULL'; 
+//   idFactura = 'null';
 }
-
   // Validación (debes implementar tu propia lógica de validación aquí)
   //idFactura && (rtncliente || rtnclienteC) && correo && telefono && tiposervicio && ubicacion && descripcion
   if (true) {
@@ -396,6 +393,7 @@ if (radio.checked) {
               );
               // Puedes realizar otras acciones después del éxito de la solicitud AJAX
           }
+          
       });
   } else {
       // Manejar la validación fallida, por ejemplo, mostrar un mensaje de error
@@ -405,7 +403,40 @@ if (radio.checked) {
           'error'
       );
   }
+  if ($('#clientenuevo').is(':checked')) {
+    let nombreN = $('#nombre').val();
+    let rtnN = $('#rntcliente').val();
+    let telefonoN = $('#telefono').val();
+    let correoN = $('#correoCliente').val();
+    let direccionN = $('#direccion').val();
+    //validado
+    if (true) {
+      $.ajax({
+        url: "../../../Vista/crud/carteraCliente/nuevoCliente.php",
+        type: "POST",
+        datatype: "JSON",
+        data: {
+          nombre: nombreN,
+          rtn: rtnN,
+          telefono: telefonoN,
+          correo: correoN,
+          direccion: direccionN
+        },
+        success: function () {
+        }
+      });
+  
+    }
+  }
 });
+
+
+//Guardar en Cartera Cliente
+// $('#form-solicitud').submit(function (e) {
+//   e.preventDefault(); // Evita el comportamiento normal del submit, es decir, la recarga total de la página
+//   // Verifica si el radio "Nuevo Cliente" está seleccionado
+ 
+// });
 
 
 
