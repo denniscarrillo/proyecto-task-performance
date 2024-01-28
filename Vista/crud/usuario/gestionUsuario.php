284716f11170e54e9f8bc3793a6a87a25e3bf9b3
Dennis Carrillo <dennis.carrillo@unah.hn>
2024-01-27 22:14:57 -0600

    Se fixeo el error en editar tarea - Productos de interes que al seleccionar varios producto estos a veces se insertaban todo y a veces no. Se comenzo con el nuevo diseño de datables en algunos CRUD

--- Vista/crud/usuario/gestionUsuario.php
+++ Vista/crud/usuario/gestionUsuario.php
@@ -150,9 +150,8 @@ if (isset($_SESSION['usuario'])) {
             <!-- <a href="../../../TCPDF/examples/reporteriaListaUsuario.php" target="_blank" class="btn_Pdf btn btn-primary hidden" id="btn_Pdf"> <i class="fas fa-file-pdf"> </i> Generar PDF</a> -->
             <button class="btn_Pdf btn btn-primary hidden" id="btn_Pdf"> <i class="fas fa-file-pdf"></i> Generar
               PDF</button>
-
           </div>
-          <table class="table" id="table-Usuarios">
+          <table class="display nowrap table" id="table-Usuarios" style="width:100%">
             <thead>
               <tr>
                 <th scope="col"> ID </th>
