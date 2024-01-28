6467054b8825aa106ee69039616512abe2049b3f
Dennis Carrillo <dennis.carrillo@unah.hn>
2024-01-27 23:57:34 -0600

    Fix dataTable solicitud

--- Vista/crud/DataTableSolicitud/gestionDataTableSolicitud.php
+++ Vista/crud/DataTableSolicitud/gestionDataTableSolicitud.php
@@ -155,7 +155,7 @@ if (isset($_SESSION['usuario'])) {
             <button class="btn_Pdf btn btn-primary hidden" id="btn_Pdf"> <i class="fas fa-file-pdf"></i> Generar
               PDF</button>
           </div>
-          <table class="table" id="table-Solicitud">
+          <table class="display nowrap table" id="table-Solicitud" style="width:100%">
             <thead>
               <tr>
                 <th scope="col"> ID </th>
