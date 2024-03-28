cb528a9de4850de33a94879c0246d66fa3a65f30
danielamartinez08 <estefanimartinez@unah.hn>
2024-03-27 21:58:44 -0600

    Se elimino el ; que se genero al combinar los cambios para resolver los conflictos ya que no se estaba eliminando la venta

--- Recursos/js/Ventas/datatable.js
+++ Recursos/js/Ventas/datatable.js
@@ -137,7 +137,7 @@ $(document).on("click", "#btn_editar", function () {
 
 $(document).on("click", "#btn_eliminar", function () {
   let fila = $(this).closest("tr"),
-    factura = $(this).closest("tr").find("td:eq(0)").text();, //capturo el ID
+    factura = $(this).closest("tr").find("td:eq(0)").text(), //capturo el ID
     cliente = $(this).closest("tr").find("td:eq(1)").text(),
     rtn = $(this).closest("tr").find("td:eq(2)").text();
   Swal.fire({
