b24823645cbe855beacd048521b0432608746dda
user <melany.torres@unah.hn>
2024-04-03 21:43:52 -0600

    Se corrigio envio de correo en el mantenimiento usuario

--- Vista/crud/usuario/enviarCorreoNuevoUsuario.php
+++ Vista/crud/usuario/enviarCorreoNuevoUsuario.php
@@ -18,23 +18,23 @@ function enviarCorreoNuevoUsuario($destinario, $usuario, $contrasenia){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = $getDataServerEmail[1]['valorParametro'];                //SMTP username
-        $mail->Password   = $getDataServerEmail[2]['valorParametro'];                      //SMTP password
+        $mail->Username   = $getDataServerEmail['ADMIN_CORREO'];                      //SMTP username
+        $mail->Password   = $getDataServerEmail['ADMIN_PASSWORD'];                       //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = intval($getDataServerEmail[3]['valorParametro']);  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        $mail->Port       = intval($getDataServerEmail['ADMIN_PUERTO']);    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
        //Recipients
-       $mail->setFrom($getDataServerEmail[1]['valorParametro'], $getDataServerEmail[0]['valorParametro']);
+       $mail->setFrom($getDataServerEmail['ADMIN_CORREO'], $getDataServerEmail['ADMIN_USER']);
        $mail->addAddress($destinario);                             //Add a recipient
         //Content
         $mail->isHTML(true);                                        //Set email format to HTML
-        $mail->Subject = 'Creación de cuenta para poder acceder al sistema Equipos&Cocinas';
+        $mail->Subject = 'Credenciales de acceso al sistema Task Performance ';
         $mail->Body    = 
         '<div>
             <h2>Se le notifica que ha sido creada su cuenta con éxito</h2>
             <h1>Ahora puede iniciar sesión</h1>
-            <p><b> Usuario:'.$usuario.'</b></p>
-            <p><b> Contraseña:'.$contrasenia.'</b></p>
+            <p><b> Usuario: ' .$usuario.'</b></p>
+            <p><b> Contraseña: ' .$contrasenia.'</b></p>
         </div>';
         // $mail->AltBody = 'Si funcionó!';
         $mail->CharSet = 'UTF-8'; // Setear UTF-8 para caracteres especiales
